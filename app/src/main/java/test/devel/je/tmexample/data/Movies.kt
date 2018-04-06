6f29a4a6f75eabcc2343b94251db1ccbe64ae591
JienLee <je94451@womanstalk.co.kr>
2018-04-06 21:46:52 +0900

    movie 결과

---
+++ app/src/main/java/test/devel/je/tmexample/data/Movies.kt
@@ -0,0 +1,18 @@
+package test.devel.je.tmexample.data
+
+object Movies {
+    var vote_count:Int = 0
+    var id : Int = 0
+    var video : Boolean = false
+    var vote_average : Float = 0f
+    var title : String = ""
+    var popularity : Double = 0.0
+    var poster_path : String = ""
+    var original_language : String = ""
+    var original_title : String = ""
+    var genre_ids: IntArray = intArrayOf()
+    var backdrop_path : String = ""
+    var adult : Boolean = false
+    var overview : String = ""
+    var release_date : String = ""
+}
