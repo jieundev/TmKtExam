1d10d67264fefe1f8081ebf24101338f8cb48885
JienLee <je94451@womanstalk.co.kr>
2018-04-06 23:40:22 +0900

    data 수정

--- app/src/main/java/test/devel/je/tmexample/data/Movies.kt
+++ app/src/main/java/test/devel/je/tmexample/data/Movies.kt
@@ -1,18 +1,18 @@
 package test.devel.je.tmexample.data
 
-object Movies {
-    var vote_count:Int = 0
-    var id : Int = 0
-    var video : Boolean = false
-    var vote_average : Float = 0f
-    var title : String = ""
-    var popularity : Double = 0.0
-    var poster_path : String = ""
-    var original_language : String = ""
-    var original_title : String = ""
-    var genre_ids: IntArray = intArrayOf()
-    var backdrop_path : String = ""
-    var adult : Boolean = false
-    var overview : String = ""
-    var release_date : String = ""
-}
+data class Movies(
+    val vote_count:Int,
+    val id : Int,
+    val video : Boolean,
+    val vote_average : Float,
+    val title : String,
+    val popularity : Double,
+    val poster_path : String,
+    val original_language : String,
+    val original_title : String,
+    val genre_ids: IntArray,
+    val backdrop_path : String,
+    val adult : Boolean,
+    val overview : String,
+    val release_date : String
+)
